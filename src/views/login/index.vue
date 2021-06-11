6650a1f4d954c25a80e28952f1994fa653e4d780
BoatingZeng <945467442@qq.com>
2021-06-11 10:24:27 +0800

    去掉写死的账号密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -96,8 +96,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'zengdeting',
-        password: 'boatingzeng233'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
